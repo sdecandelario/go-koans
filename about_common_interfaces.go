53ca24cb327df334f025383cde14ac8bb7aacf3a
Sergio <sergio.decandelario@gmail.com>
2017-11-25 13:55:00 +0100

    About common interfaces tests passed

--- about_common_interfaces.go
+++ about_common_interfaces.go
@@ -18,6 +18,8 @@ func aboutCommonInterfaces() {
 		   $ open http://localhost:8080/pkg/bytes/
 		*/
 
+		out.WriteString(in.String())
+
 		assert(out.String() == "hello world") // get data from the io.Reader to the io.Writer
 	}
 
@@ -26,6 +28,8 @@ func aboutCommonInterfaces() {
 		in.WriteString("hello world")
 
 		out := new(bytes.Buffer)
+
+		out.WriteString(in.String()[:5])
 
 		assert(out.String() == "hello") // duplicate only a portion of the io.Reader
 	}
